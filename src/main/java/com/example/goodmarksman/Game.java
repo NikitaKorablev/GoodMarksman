c598301d0723a955fa0c9822d7cd04375aa99f40
Никита Кораблев <kit06092003@gmail.com>
2024-02-17 13:32:57 +0300

    first realization

--- src/main/java/com/example/goodmarksman/Game.java
+++ src/main/java/com/example/goodmarksman/Game.java
@@ -1,9 +1,14 @@
 package com.example.goodmarksman;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+
+import java.lang.Math;
 
 public class Game {
     @FXML
@@ -14,6 +19,10 @@ public class Game {
     private Circle smallTarget;
     @FXML
     private Polygon arrow;
+    @FXML
+    private Text shots;
+    @FXML
+    private Text score;
 
     private double upperThreshold;
     private double lowerThreshold;
@@ -21,10 +30,27 @@ public class Game {
     private boolean isShooting = false;
     private int bigTargetOrientation = 1;
     private int smallTargetOrientation = -1;
+
+    private final int arrowSpeed = 10;
+    private final int targetSpeed = 1;
+
+    private void moveArrowToStart() { arrow.setLayoutX(38); }
+    private void colorizeBigTarget(Color color) { bigTarget.setFill(color); }
+    private void colorizeSmallTarget(Color color) { smallTarget.setFill(color); }
+
+    private void addScore(int add) { score.setText(Integer.toString(Integer.parseInt(score.getText())+add)); }
+    private void addShots() { shots.setText(Integer.toString(Integer.parseInt(shots.getText())+1)); }
     private void startView() {
+        double[] sep = {bigTarget.getLayoutX() - bigTarget.getRadius(),
+                        bigTarget.getLayoutX(),
+                        smallTarget.getLayoutX() - smallTarget.getRadius(),
+                        smallTarget.getLayoutX()};
+
         Thread thread = new Thread(() -> {
-            try {
-                while(!isPaused) {
+            while(!isPaused) {
+                Platform.runLater(() -> {
+                    colorizeBigTarget(Color.rgb(33, 212, 255));
+                    colorizeSmallTarget(Color.rgb(255, 33, 33));
                     if (bigTargetOrientation == 1 & bigTarget.getLayoutY() == lowerThreshold - bigTarget.getRadius())
                         bigTargetOrientation = -1;
                     else if (bigTargetOrientation == -1 & bigTarget.getLayoutY() == upperThreshold + bigTarget.getRadius())
@@ -35,22 +61,44 @@ public class Game {
                     else if (smallTargetOrientation == -1 & smallTarget.getLayoutY() == upperThreshold + smallTarget.getRadius())
                         smallTargetOrientation = 1;
 
-                    bigTarget.setLayoutY(bigTarget.getLayoutY() + bigTargetOrientation);
-                    smallTarget.setLayoutY(smallTarget.getLayoutY() + smallTargetOrientation);
+                    bigTarget.setLayoutY(bigTarget.getLayoutY() + bigTargetOrientation * targetSpeed);
+                    smallTarget.setLayoutY(smallTarget.getLayoutY() + smallTargetOrientation * targetSpeed);
 
                     if (isShooting) {
-                        if (arrow.getLayoutX() >= 410) {
-                            arrow.setLayoutX(60);
-                            isShooting = false;
-                            System.out.println(arrow.getPoints());
+                        if (arrow.getLayoutX() >= sep[0] & arrow.getLayoutX() <= sep[1]) {
+                            double distance = Math.sqrt(Math.pow(sep[1] - arrow.getLayoutX(), 2) +
+                                    Math.pow(arrow.getLayoutY() - bigTarget.getLayoutY(), 2));
+                            if (distance <= bigTarget.getRadius()) {
+                                colorizeBigTarget(Color.rgb(0, 128, 0));
+                                isShooting = false;
+                                addScore(1);
+                                moveArrowToStart();
+                            }
+                        } else if (arrow.getLayoutX() >= sep[2] & arrow.getLayoutX() <= sep[3]) {
+                            double distance = Math.sqrt(Math.pow(sep[3] - arrow.getLayoutX(), 2) +
+                                    Math.pow(arrow.getLayoutY() - smallTarget.getLayoutY(), 2));
+                            if (distance <= smallTarget.getRadius()) {
+                                isShooting = false;
+                                addScore(2);
+                                colorizeSmallTarget(Color.rgb(0, 128, 0));
+                                moveArrowToStart();
+                            }
                         }
-                        arrow.setLayoutX(arrow.getLayoutX() + 10);
+
+                        else if (arrow.getLayoutX() >= 380) {
+                            moveArrowToStart();
+                            isShooting = false;
+                        }
+                        arrow.setLayoutX(arrow.getLayoutX() + arrowSpeed);
                     }
+                });
+                try {
                     Thread.sleep(10);
+                } catch(InterruptedException err) {
+                    System.err.println(err);
                 }
-            } catch(InterruptedException err) {
-                System.err.println(err);
             }
+
         });
 
         thread.setDaemon(true);
@@ -71,5 +119,8 @@ public class Game {
         isPaused = true;
     }
     @FXML
-    protected void onShootButtonClick() { isShooting = true; }
+    protected void onShotButtonClick() {
+        addShots();
+        isShooting = true;
+    }
 }
